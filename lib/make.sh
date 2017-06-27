89f59cee7b225307ee9eda9e9a0f66f6de8b52b2
CD Athuraliya <cdathuraliya@gmail.com>
2017-06-27 12:09:46 +0530

    Update make.sh
    Fixes for following errors,
    tensorflow.python.framework.errors_impl.NotFoundError: lib/roi_pooling_layer/roi_pooling.so: undefined symbol: _ZN10tensorflow7strings6StrCatB5cxx11ERKNS0_8AlphaNumE
    tensorflow.python.framework.errors_impl.NotFoundError: lib/psroi_pooling_layer/psroi_pooling.so: undefined symbol: _ZN10tensorflow7strings6StrCatB5cxx11ERKNS0_8AlphaNumE

--- lib/make.sh
+++ lib/make.sh
@@ -15,7 +15,8 @@ nvcc -std=c++11 -c -o roi_pooling_op.cu.o roi_pooling_op_gpu.cu.cc \
 
 # for gcc5-built tf
 g++ -std=c++11 -shared -D_GLIBCXX_USE_CXX11_ABI=1 -o roi_pooling.so roi_pooling_op.cc \
-	roi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
+	roi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64 \
+	-D_GLIBCXX_USE_CXX11_ABI=0
 cd ..
 
 
@@ -25,7 +26,8 @@ nvcc -std=c++11 -c -o psroi_pooling_op.cu.o psroi_pooling_op_gpu.cu.cc \
 	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_52
 
 g++ -std=c++11 -shared -o psroi_pooling.so psroi_pooling_op.cc \
-	psroi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
+	psroi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64 \
+	-D_GLIBCXX_USE_CXX11_ABI=0
 
 ## if you install tf using already-built binary, or gcc version 4.x, uncomment the two lines below
 #g++ -std=c++11 -shared -D_GLIBCXX_USE_CXX11_ABI=0 -o psroi_pooling.so psroi_pooling_op.cc \
